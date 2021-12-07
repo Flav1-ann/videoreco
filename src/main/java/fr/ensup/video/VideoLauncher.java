eeb2184a10de7db56e20b6436801e719f57703e6
flav1-ann <Moitoi28>
2021-12-07 16:44:00 +0100

    jpa

--- src/main/java/fr/ensup/video/VideoLauncher.java
+++ src/main/java/fr/ensup/video/VideoLauncher.java
@@ -5,16 +5,41 @@ import fr.ensup.video.repository.VideoJdbcTemplate;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
 public class VideoLauncher {
     public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
-        VideoJdbcTemplate videoJdbcTemplate = (VideoJdbcTemplate) context.getBean("videoJdbcTemplate");
+        //SANS JPA
+//        ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+//        VideoJdbcTemplate videoJdbcTemplate = (VideoJdbcTemplate) context.getBean("videoJdbcTemplate");
+//
+////        Video video =  new Video(1,"Matrix","film");
+////        videoJdbcTemplate.save(video);
+////        System.out.println("Une video à été crée : " + video);
+//
+//        System.out.println(videoJdbcTemplate.get(1));
+//        System.out.println(videoJdbcTemplate.getAll());
 
-//        Video video =  new Video(1,"Matrix","film");
-//        videoJdbcTemplate.save(video);
-//        System.out.println("Une video à été crée : " + video);
 
-        System.out.println(videoJdbcTemplate.get(1));
-        System.out.println(videoJdbcTemplate.getAll());
+        //AVEC JPA
+
+        //1-ouverture unite de connection avec JPA
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("video-reco-jpa");
+        EntityManager em = emf.createEntityManager();
+        //2- Ouverture d'une transaction
+        EntityTransaction tr = em.getTransaction();
+        tr.begin();
+        //3- on instancie d'une vidéo
+        Video video =  new Video("naruto","anim");
+        //4- on persiste l'objets relationnel
+        em.persist(video);
+        //5- on commit
+        tr.commit();
+        //6- on ferme
+        em.close();
+        emf.close();
     }
 }
--- src/main/java/fr/ensup/video/model/Video.java
+++ src/main/java/fr/ensup/video/model/Video.java
@@ -1,9 +1,27 @@
 package fr.ensup.video.model;
 
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Video {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO,generator="native")
+    @GenericGenerator(name = "native",strategy = "native")
     private long id;
+
     private String title;
+
     private  String type;
+
+    public Video(String title, String type) {
+        this.title = title;
+        this.type = type;
+    }
 
     @Override
     public String toString() {
